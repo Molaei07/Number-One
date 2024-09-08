ba3ea883a0ac4405f9d3b7a47878ed6b0169dc77
Molaei <molaie.web.developer@gmail.com>
2024-09-08 07:17:55 +0100

    add checkbox and set placeholder to field

--- src/components/common/Form/FormInput.jsx
+++ src/components/common/Form/FormInput.jsx
@@ -7,20 +7,20 @@ import DatePicker from "react-multi-date-picker"
 import "./FormDate.css"
 import { useTranslation } from "react-i18next"
 
-const FormInput = ({ sectionName, certificate, type, options, fullSize, dir, style, variants }) => {
+const FormInput = ({ sectionName, certificate, type, options, fullSize, dir, style, variants, fieldStyle, eyeStyle, placeholder }) => {
     const [visible, setVisible] = useState(false)
     const { i18n } = useTranslation()
     const fieldVariants = {
         password: (
             <div className="w-full relative">
-                <Field dir={dir} type={visible ? "text" : "password"} name={certificate} id={certificate} className="fieldsStyle" />
-                <div onClick={() => { setVisible(!visible) }} className="w-6 h-4 absolute bottom-3 right-4">
+                <Field dir={dir} type={visible ? "text" : "password"} placeholder={placeholder} name={certificate} id={certificate} className={`fieldsStyle ${fieldStyle}`} />
+                <div onClick={() => { setVisible(!visible) }} className={`w-6 h-4 cursor-pointer absolute bottom-3 right-4 ${eyeStyle}`}>
                     {visible ? <EyeIcon /> : <UnEyeIcon width="20" />}
                 </div>
             </div>
         ),
         simple: (
-            <Field dir={dir} type={type} name={certificate} id={certificate} className="fieldsStyle" />
+            <Field dir={dir} placeholder={placeholder} type={type} name={certificate} id={certificate} className={`fieldsStyle ${fieldStyle}`} />
         ),
         select: (
             <Field as="select" name={certificate} id={certificate} className="fieldsStyle w-full min-h-10 max-h-40">
@@ -28,7 +28,7 @@ const FormInput = ({ sectionName, certificate, type, options, fullSize, dir, sty
             </Field>
         ),
         area: (
-            <Field as="textarea" name={certificate} id={certificate} className="fieldsStyle w-full min-h-10 max-h-40 h-40" />
+            <Field as="textarea" placeholder={placeholder} name={certificate} id={certificate} className={`fieldsStyle w-full min-h-10 max-h-40 h-40 ${fieldStyle}`} />
         ),
         date: (
             <DatePicker
@@ -36,13 +36,19 @@ const FormInput = ({ sectionName, certificate, type, options, fullSize, dir, sty
                 locale={persian_fa}
                 containerStyle={{ width: "100%", direction: i18n.language == "en" ? "ltr" : "ltr" }}
             />
+        ),
+        checkbox: (
+            <label htmlFor={certificate} className={`w-fit flex gap-x-2 items-center cursor-pointer ${fieldStyle}`}>
+                <Field type="checkbox" name={certificate} id={certificate} />
+                {placeholder}
+            </label>
         )
     }
     return (
         <div className={`flex flex-wrap h-fit ${fullSize ? "w-full" : style ? style : "w-full sm:w-[45%]"}`}>
             <div className="relative w-full text-nowrap px-3 flex items-end gap-x-2 justify-between text-sm text-red-600">
                 <label htmlFor={certificate} className="text-base text-neutral-400">{sectionName}</label>
-                <span className="w-full bg-white absolute top-1 right-3">
+                <span className={`w-full bg-white top-1 right-3 ${sectionName ? "absolute" : ""}`}>
                     <ErrorMessage name={certificate} />
                 </span>
             </div>
